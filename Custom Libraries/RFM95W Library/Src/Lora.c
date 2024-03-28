3d911d548aaf9ad5d51f47d8d21611940aaa44a7
Polo280 <jorglezd28@gmail.com>
2024-03-28 08:49:29 -0600

    Currently only working as transmitter mode

---
+++ Custom Libraries/RFM95W Library/Src/Lora.c
@@ -0,0 +1,280 @@
+#include <Lora.h>
+
+// SPI Handler
+SPI_HandleTypeDef *spi_handler;
+
+// Data buffer for SPI communication
+uint8_t data_buffer [2];
+
+// Default initialization parameters
+LoraConfigStruct defaultConfigStruct= {
+    .spi_port = LORA_DEFAULT_SPI_PORT,
+    .nss_port = LORA_DEFAULT_NSS_PORT,
+    .rst_port = LORA_DEFAULT_RST_PORT,
+    .mosi_pin = LORA_DEFAULT_MOSI_PIN,
+    .miso_pin = LORA_DEFAULT_MISO_PIN,
+    .nss_pin = LORA_DEFAULT_NSS_PIN,
+    .sck_pin = LORA_DEFAULT_SCK_PIN,
+    .rst_pin = LORA_DEFAULT_RST_PIN,
+	.freq =    LORA_DEFAULT_FREQUENCY
+};
+
+// Custom Initialization parameters
+LoraConfigStruct *loraConfigStruct;
+
+void LoraGPIOsConfig(LoraConfigStruct *configStruct){
+    // NOTE: This function assumes you have previously enabled port RCC clock from the GPIOs used with LoRa Module
+    // Initialization of SPI channels
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
+    GPIO_InitStruct.Pin = configStruct->miso_pin | configStruct->mosi_pin | configStruct->sck_pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(configStruct->spi_port, &GPIO_InitStruct);
+
+    // Initialization of NSS line
+    GPIO_InitStruct.Pin = configStruct->nss_pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(configStruct->nss_port, &GPIO_InitStruct);
+
+    // Initialization of RST Line
+    GPIO_InitStruct.Pin = configStruct->rst_pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(configStruct->rst_port, &GPIO_InitStruct);
+}
+
+// Intialize Lora module with default values
+uint8_t LoraDefaultInit(void){
+	loraConfigStruct = &defaultConfigStruct;
+    return LoraInit(&defaultConfigStruct);
+}
+
+uint8_t LoraInit(LoraConfigStruct *loraConfig){
+    // Configure GPIOS
+    LoraGPIOsConfig(loraConfig);
+
+    // Hardware Reset  (Optional but recommended)
+    HAL_GPIO_WritePin(loraConfig->rst_port, loraConfig->rst_pin, GPIO_PIN_SET);  // RESET IS LOW OR HIGH?
+    HAL_Delay(200);
+    HAL_GPIO_WritePin(loraConfig->rst_port, loraConfig->rst_pin, GPIO_PIN_RESET);
+    HAL_Delay(200);
+    HAL_GPIO_WritePin(loraConfig->rst_port, loraConfig->rst_pin, GPIO_PIN_SET);
+    HAL_Delay(50);
+
+    // Set NSS to high (not communicating with module currently)
+    HAL_GPIO_WritePin(loraConfig->nss_port, loraConfig->nss_pin, GPIO_PIN_SET);
+
+    // Begin SPI communication in specified handler
+    if(HAL_SPI_Init(spi_handler) != HAL_OK){
+        Error_Handler();
+    }
+
+    // Get and verify LoRa version
+    if(GetLoraVersion() == 0){
+    	return LORA_NOT_OK;  // Error
+    }
+
+    // Set to sleep to modify freq and other values
+    LoraSleep();
+    // Set Lora frequency
+    LoraSetFrequency(loraConfig->freq);
+
+    // Configure RX and TX FIFOs
+    LoraWriteRegister(REG_FIFO_TX_BASE_ADDR, 0);
+    LoraWriteRegister(REG_FIFO_RX_BASE_ADDR, 0);
+
+    // set LNA boost
+    LoraWriteRegister(REG_LNA, LoraReadRegister(REG_LNA) | 0x03);
+    // set auto AGC
+    LoraWriteRegister(REG_MODEM_CONFIG_3, 0x04);
+    // Set transmission power
+    LoraSetTxPower(17, PA_BOOST_MODE);
+
+    LoraSetIdle();
+    return LORA_OK;
+}
+
+// Function to set a previously configured SPI handler (USE IT BEFORE ANY OTHER FUNCTION)
+void LoraSetSPIHandler(SPI_HandleTypeDef *hspi){
+    spi_handler = hspi;
+}
+
+// Function to send some address and a data byte to the module
+uint8_t LoraSendSPI(uint8_t address, uint8_t value, LoraConfigStruct *loraConfig){
+    uint8_t response = 0;
+
+    HAL_GPIO_WritePin(loraConfig->nss_port, loraConfig->nss_pin, GPIO_PIN_RESET);          // Set to low the nss line to enable module for sending data to it
+    if(HAL_SPI_Transmit(spi_handler, &address, 1, SPI_TRANSMIT_TIMEOUT)!= HAL_OK){Error_Handler();}                   // Send the address
+    if(HAL_SPI_TransmitReceive(spi_handler, &value, &response, 1, SPI_TRANSMIT_TIMEOUT) != HAL_OK){Error_Handler();}    // The 1 is the number of bytes you want to transmit
+    HAL_GPIO_WritePin(loraConfig->nss_port, loraConfig->nss_pin, GPIO_PIN_SET);        // Set nss line low to disable
+    return response;
+}
+
+// Function to read registers from LoRa module
+uint8_t LoraReadRegister(uint8_t address){
+    return LoraSendSPI(address & 0x7f, 0x00, loraConfigStruct);
+}
+
+// Function to write data to registers
+void LoraWriteRegister(uint8_t address, uint8_t value){
+    LoraSendSPI(address | 0x80, value, loraConfigStruct);
+}
+
+/////////////////////// FUNCTIONS USING REGISTER FUNCTIONALITY ///////////////////////
+
+// Prepare to transmit
+void LoraTransmit(const char *buffer){
+	LoraWrite((uint8_t *)buffer, strlen(buffer));
+}
+
+// Function to actually transmit the data
+void LoraWrite(const uint8_t *buffer, size_t size)  // Size_t is the same as unsigned long
+{
+  int currentLength = LoraReadRegister(REG_PAYLOAD_LENGTH);
+
+  // Ensure total size is less than packet max allowed length
+  if ((currentLength + size) > MAX_PKT_LENGTH) {
+    size = MAX_PKT_LENGTH - currentLength;
+  }
+
+  // write data
+  for (size_t i = 0; i < size; i++) {
+    LoraWriteRegister(REG_FIFO, buffer[i]);
+  }
+
+  // update length
+  LoraWriteRegister(REG_PAYLOAD_LENGTH, currentLength + size);
+}
+
+// Function to get the version of the LoRa module
+uint8_t GetLoraVersion(void){
+    return LoraReadRegister(REG_VERSION);
+}
+
+uint8_t LoraGetRXBytes(void){
+	return LoraReadRegister(REG_RX_NB_BYTES);
+}
+
+// Expecting 915 MHz frequency (915E6)
+void LoraSetFrequency(uint64_t frequency){  				// Configure FRF (Frequency Synthesis Register)
+	uint64_t frf = ((uint64_t)frequency << 19) / 32000000;  // Given by datasheet formula
+	LoraWriteRegister(REG_FRF_MSB, (uint8_t)(frf >> 16));
+	LoraWriteRegister(REG_FRF_MID, (uint8_t)(frf >> 8));
+	LoraWriteRegister(REG_FRF_LSB, (uint8_t)(frf >> 0));
+}
+
+// Configure packet format before sending
+uint8_t LoraBeginPacket(uint8_t isImplicitHeader){   // Explicit header messages contain a header for detecting errors, implicits dont
+	if(IsTransmitting() == 1){
+		return 0;
+	}
+
+	// Set in idle (sleep but with internal oscillators enabled)
+	LoraSetIdle();
+
+	// Handle implicit and explicit headers
+	if(isImplicitHeader == 1){
+		LoraWriteRegister(REG_MODEM_CONFIG_1, LoraReadRegister(REG_MODEM_CONFIG_1) | 0x01);
+	}else if(isImplicitHeader == 0){
+		LoraWriteRegister(REG_MODEM_CONFIG_1, LoraReadRegister(REG_MODEM_CONFIG_1) & 0xfe);
+	}
+
+	// reset FIFO address and payload length
+    LoraWriteRegister(REG_FIFO_ADDR_PTR, 0);
+    LoraWriteRegister(REG_PAYLOAD_LENGTH, 0);
+    return 1;
+}
+
+uint8_t LoraEndPacket(uint8_t sync){
+	// For now, only synchronous (wait for transmission completed by polling) tx is supported
+	  LoraWriteRegister(REG_OP_MODE, MODE_LONG_RANGE_MODE | MODE_TX);   // Enable TX mode
+
+	  while ((LoraReadRegister(REG_IRQ_FLAGS) & IRQ_TX_DONE_MASK) == 0) {
+	        HAL_Delay(1);
+	  }
+	  // clear IRQ's
+	  LoraWriteRegister(REG_IRQ_FLAGS, IRQ_TX_DONE_MASK);
+	  return 0;
+}
+
+uint8_t LoraGetSpreadingFactor(void){
+	return LoraReadRegister(REG_MODEM_CONFIG_2) >> 4;
+}
+
+// Function to check if module is currently transmitting info
+uint8_t IsTransmitting(void){
+	if ((LoraReadRegister(REG_OP_MODE) & MODE_TX) == MODE_TX) {
+	    return 1;
+	}
+
+	if (LoraReadRegister(REG_IRQ_FLAGS) & IRQ_TX_DONE_MASK) {
+	  // clear IRQ's
+	  LoraWriteRegister(REG_IRQ_FLAGS, IRQ_TX_DONE_MASK);
+	}
+	return 0;
+}
+
+// Set OverCurrent Protection
+void LoraSetOCP(uint8_t milliAmps){
+	uint8_t OCP_Trim = 27;   // 27 allows for maximum safe current protection (240 mA)
+
+	// Adjust according to datasheet formula
+	if (milliAmps <= 120) {
+		OCP_Trim = (milliAmps - 45) / 5;
+	} else if (milliAmps <= 240) {
+	    OCP_Trim = (milliAmps + 30) / 10;
+	}
+	LoraWriteRegister(REG_OCP, 0x20 | (0x1F & OCP_Trim));
+}
+
+// Set transmission power (mode is used as a boolean, where 0 means RFO = Low power applications and 1 means PA_BOOST used for higher power applications
+void LoraSetTxPower(uint8_t level, uint8_t mode){
+	if(mode == RFO_MODE){
+		// Bound level
+		if(level < 0){
+			level = 0;
+		}else if(level > 14){
+			level = 14;
+		}
+		LoraWriteRegister(REG_PA_CONFIG, 0x70 | level);
+	}else{
+		// PA_BOOST_MODE
+		if (level > 17) {
+		   if (level > 20) {
+		      level = 20;
+		   }
+		   // subtract 3 from level, so 18 - 20 maps to 15 - 17
+		   level -= 3;
+		   // High Power +20 dBm Operation (Semtech SX1276/77/78/79 5.4.3.)
+		   LoraWriteRegister(REG_PA_DAC, 0x87);
+		   LoraSetOCP(140);
+		}else {
+	      if (level < 2) {
+	        level = 2;
+	      }
+	      //Default value PA_HF/LF or +17dBm
+	      LoraWriteRegister(REG_PA_DAC, 0x84);
+	      LoraSetOCP(100);
+	    }
+		LoraWriteRegister(REG_PA_CONFIG, PA_BOOST | (level - 2));
+	}
+}
+
+// Set Lora Module to IDLE mode
+void LoraSetIdle(void){
+    LoraWriteRegister(REG_OP_MODE, MODE_LONG_RANGE_MODE | MODE_STDBY);
+}
+
+// Set Lora to sleep mode
+void LoraSleep(void){
+   LoraWriteRegister(REG_OP_MODE, MODE_LONG_RANGE_MODE | MODE_SLEEP);
+}
+
+// Put the Lora module to sleep if not needed anymore
+void LoraEnd(void){
+	LoraSleep();
+}
+
---
+++ Custom Libraries/RFM95W Library/Src/main.c
@@ -0,0 +1,193 @@
+/*
+Code implemented in NUCLEO F303RE
+*/
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private variables ---------------------------------------------------------*/
+// Handlers
+SPI_HandleTypeDef hspi1;
+UART_HandleTypeDef huart2;
+
+// Control variables
+uint32_t blink_aux = 0;
+uint32_t lora_aux = 0;
+uint8_t counter = 0;
+
+// Lora Transmission
+uint8_t buffer[64];
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+static void MX_SPI1_Init(void);
+
+int main(void)
+{
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_SPI1_Init();
+
+  // Set Lora SPI handler
+  LoraSetSPIHandler(&hspi1);
+  // Default Init
+  if(LoraDefaultInit() != 0){
+	  Error_Handler();
+  }
+
+  // Test send data
+  snprintf((char *) buffer, sizeof(buffer), "Hello World");
+  LoraBeginPacket(0);
+  LoraTransmit((char *)buffer);
+  LoraEndPacket(0);
+
+  /* Infinite loop */
+  while (1)
+  {
+	  // Toggle pin
+	  if(HAL_GetTick() - blink_aux >= TIME_TO_BLINK){
+		  HAL_GPIO_TogglePin(LED_PORT, LED_PIN);
+		  blink_aux = HAL_GetTick();
+	  }
+
+	  // Transmit count number
+	  if(HAL_GetTick() - lora_aux >= TIME_TO_TRANSMIT){
+		  snprintf((char *) buffer, sizeof(buffer), "Count = %d", counter);
+		  // Transmit
+		  LoraBeginPacket(0);
+		  LoraTransmit((char *)buffer);
+		  LoraEndPacket(0);
+
+		  // Bound count to 255
+		  if(counter <= 254)
+			  counter ++;
+		  else
+			  counter = 0;
+		  lora_aux = HAL_GetTick();
+	  }
+  }
+}
+
+
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  RCC_OscInitStruct.PLL.PREDIV = RCC_PREDIV_DIV1;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_USART2;
+  PeriphClkInit.Usart2ClockSelection = RCC_USART2CLKSOURCE_PCLK1;
+  if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+static void MX_SPI1_Init(void)
+{
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_8;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 7;
+  hspi1.Init.CRCLength = SPI_CRC_LENGTH_DATASIZE;
+  hspi1.Init.NSSPMode = SPI_NSS_PULSE_DISABLE;
+
+  // NO INIT (this is done in the library)
+
+}
+
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE END USART2_Init 1 */
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = 9600;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  // LED Init
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Pin = GPIO_PIN_8;
+  HAL_GPIO_Init(LED_PORT, &GPIO_InitStruct);
+
+}
+
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+	  HAL_GPIO_WritePin(LED_PORT, LED_PIN, GPIO_PIN_SET);
+  }
+}
